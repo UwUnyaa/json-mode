1c766ad273253ec0aa816e02be8d6e726ed25a62
DoMiNeLa10 <retrogamingdomi@wp.pl>
2018-08-24 22:17:01 +0200

    clean up condition for pretty printing and simplify code

--- json-mode.el
+++ json-mode.el
@@ -87,9 +87,8 @@
 ;;;###autoload
 (define-derived-mode json-mode js-mode json-mode-mode-name
   "A simple mode for JSON editing."
-  (when json-mode-pretty-print-on-open
-    (when (json-mode-buffer-valid-p)
-      (json-mode-pretty-print-buffer))
+  (when (and json-mode-pretty-print-on-open (json-mode-buffer-valid-p))
+    (json-mode-pretty-print-buffer)
     (goto-char (point-min))    ; this line is ignored in pretty print function
     (set-buffer-modified-p nil))
   (when json-mode-timer-enable
